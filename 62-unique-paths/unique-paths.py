6f66a5f61cafc1922707e35576344416201bf9fc
PATHAN MOHAMMAD SAADH <pmdsaadh723@gmail.com>
2026-08-30 16:47:15 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (42.86%) - LeetSync

---
+++ 62-unique-paths/unique-paths.py
@@ -0,0 +1,7 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = [1] * n
+        for _ in range(m - 1):
+            for col in range(1, n):
+                dp[col] += dp[col - 1]
+        return dp[-1]
