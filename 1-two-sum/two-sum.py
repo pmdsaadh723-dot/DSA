a6087789ca9cf910740abf760ad7f291dee1e923
PATHAN MOHAMMAD SAADH <pmdsaadh723@gmail.com>
2026-09-12 12:01:49 +0530

    Time: 0 ms (100.00%) | Memory: 20.4 MB (41.60%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,13 +1,8 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        hashmap = {}
-        for i in range(len(nums)):
-            complement = target - nums[i]
-            if complement in hashmap:
-                return [hashmap[complement], i]
-            hashmap[nums[i]] = i
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
+        for i, num in enumerate(nums):
+            need = target - num
+            if need in seen:
+                return [seen[need], i]
+            seen[num] = i
